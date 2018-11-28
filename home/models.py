9fb1d9aec087e56419d1c80c30a5c6d2f419cb45
Aishwarya Prabha <aishwarya.prabha@nescode.com>
2018-11-28 16:53:26 +0530

    [Update]homepage

--- home/models.py
+++ home/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    herobanner_heading=models.CharField(max_length=100, blank=True)
+    herobanner_description=RichTextField(
+    	blank=True, 
+    	verbose_name='Bottom Description')
 
-    content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
-    ]
+    herobanner_button=models.URLField(max_length=100, db_index=True, unique=True, blank=True)
